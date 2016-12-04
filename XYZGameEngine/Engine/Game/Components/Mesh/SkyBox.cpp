c2e155dd1ffeb10931f573aeb648a79af51daf0b
上海_徐阳泽 <xuyangze@jd.com>
2016-12-04 13:31:13 +0800

    重命名

---
+++ XYZGameEngine/Engine/Game/Components/Mesh/SkyBox.cpp
@@ -0,0 +1,9 @@
+//
+//  SkyBox.cpp
+//  XYZGameEngine
+//
+//  Created by Leo on 2016/11/29.
+//  Copyright © 2016年 Leo. All rights reserved.
+//
+
+#include "SkyBox.hpp"
--- XYZGameEngine/Engine/Game/Components/Mesh/SkyBox.hpp
+++ XYZGameEngine/Engine/Game/Components/Mesh/SkyBox.hpp
@@ -1,9 +1,14 @@
 //
-//  SkyBoxMesh.cpp
+//  SkyBox.hpp
 //  XYZGameEngine
 //
 //  Created by Leo on 2016/11/29.
 //  Copyright © 2016年 Leo. All rights reserved.
 //
 
-#include "SkyBoxMesh.hpp"
+#ifndef SkyBox_hpp
+#define SkyBox_hpp
+
+#include <stdio.h>
+
+#endif /* SkyBox_hpp */
